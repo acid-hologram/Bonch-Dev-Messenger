aacc3b87f4f7bc0dde44fe42cb3b5fff96f8b49e
Igor Savinov <savinov26@gmail.com>
2019-10-20 20:18:11 +0300

    Start!

--- app/src/main/java/bonch/dev/school/UI/fragments/ChatFragmenst.kt
+++ app/src/main/java/bonch/dev/school/UI/fragments/ChatFragmenst.kt
@@ -1,4 +1,14 @@
 package bonch.dev.school.UI.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
 class ChatFragmenst {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {}
 }
--- app/src/main/java/bonch/dev/school/UI/fragments/ProfileFragment.kt
+++ app/src/main/java/bonch/dev/school/UI/fragments/ProfileFragment.kt
@@ -1,4 +1,14 @@
 package bonch.dev.school.UI.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
 class ProfileFragment {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {}
 }
