c48bb211fd981df34defee02910d58b582388809
Igor Savinov <savinov26@gmail.com>
2019-10-29 19:05:35 +0300

    layouts 1.0

--- app/src/main/java/bonch/dev/school/UI/fragments/PasswordFragment.kt
+++ app/src/main/java/bonch/dev/school/UI/fragments/PasswordFragment.kt
@@ -4,17 +4,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.Fragment
 import bonch.dev.school.R
 
 class PasswordFragment: DialogFragment() {
+    lateinit var changePasswordButton: Button
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.password_change, container, false)
+        changePasswordButton = view.findViewById(R.id.change_password_button)
+        changePasswordButton.setOnClickListener { dismiss() }
         return  view
     }
 }
